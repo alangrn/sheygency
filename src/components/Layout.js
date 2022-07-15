2e469add3e0da14a70330103e3c40fe17de1c879
alan green <alangrn@hotmail.com>
2022-07-15 16:56:48 +0100

    initial commite

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Layout() {
+  return (
+    <div>
+      <div className="footer">
+        <h1>Footer</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
